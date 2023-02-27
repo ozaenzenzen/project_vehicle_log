4778654fefef4f8137f3b7655d96b5ad420c3e12
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-02-27 22:08:02 +0700

    update support

---
+++ lib/support/app_theme.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_vehicle_log/support/app_color.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData();
+  static appThemeInit() {
+    theme = theme.copyWith(
+      primaryColor: AppColor.primary,
+      scaffoldBackgroundColor: Colors.white,
+      backgroundColor: Colors.white,
+      unselectedWidgetColor: const Color(0xffb5b5b5),
+      textTheme: TextTheme(
+        headline1: GoogleFonts.inter(
+          fontSize: 36.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        headline2: GoogleFonts.inter(
+          fontSize: 25.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        headline3: GoogleFonts.inter(
+          fontSize: 21.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        headline4: GoogleFonts.inter(
+          fontSize: 18.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        headline5: GoogleFonts.inter(
+          fontSize: 16.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        headline6: GoogleFonts.inter(
+          fontSize: 14.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        bodyText1: GoogleFonts.inter(
+          fontSize: 16.sp,
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+          fontStyle: FontStyle.normal,
+        ),
+        bodyText2: GoogleFonts.inter(
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w500,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+        caption: GoogleFonts.inter(
+          fontSize: 13.sp,
+          color: Colors.black,
+          fontStyle: FontStyle.normal,
+        ),
+      ),
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: AppColor.primary,
+        secondary: AppColor.secondary,
+        // secondaryVariant: ColorUI.secondaryVariant,
+      ),
+    );
+  }
+}
