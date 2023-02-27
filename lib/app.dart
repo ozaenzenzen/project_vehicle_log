dde28b3c290dbea1dab8edf0c99a9ad38ce19836
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-02-27 22:08:15 +0700

    update main & app

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:project_vehicle_log/presentation/home_page.dart';
+import 'package:project_vehicle_log/support/app_theme.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      // designSize: const Size(360, 690),
+      designSize: const Size(411, 869),
+      // designSize: const Size(412, 896),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        AppTheme.appThemeInit();
+        return GetMaterialApp(
+          title: 'Vehicle Logs',
+          theme: AppTheme.theme,
+          home: const HomePage(),
+        );
+      },
+    );
+  }
+}
