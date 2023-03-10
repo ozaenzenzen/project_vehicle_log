82ac276ab55762350d0f130cb88853399449a8fd
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-03-10 22:46:33 +0700

    update add vehicle page

--- lib/presentation/add_vehicle_page.dart
+++ lib/presentation/add_vehicle_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:project_vehicle_log/support/app_color.dart';
 import 'package:project_vehicle_log/support/app_theme.dart';
 
@@ -21,6 +22,33 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
             color: AppColor.white,
             fontWeight: FontWeight.w600,
           ),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: AppColor.shape,
+        padding: EdgeInsets.all(16.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Add Vehicle",
+              style: AppTheme.theme.textTheme.headline1?.copyWith(
+                // color: AppColor.text_4,
+                color: Colors.black38,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(height: 10.h),
+            Text(
+              "Add your vehicle alongside with measurement parameter",
+              style: AppTheme.theme.textTheme.headline5?.copyWith(
+                // color: AppColor.text_4,
+                color: Colors.black38,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
         ),
       ),
     );
