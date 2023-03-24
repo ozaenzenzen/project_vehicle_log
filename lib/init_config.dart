9a61f94dc36157a84c877eb02a0e777f313870d8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-03-24 09:10:37 +0700

    update init config: add app local storage

--- lib/init_config.dart
+++ lib/init_config.dart
@@ -1,10 +1,12 @@
 import 'package:project_vehicle_log/support/app_connectivity_service.dart';
 import 'package:project_vehicle_log/support/app_info.dart';
+import 'package:project_vehicle_log/support/app_local_storage.dart';
 
 class AppInitConfig {
   static Future<void> init() async {
     // AppTheme.appThemeInit();
     AppInfo.appInfoInit();
     AppConnectivityService.init();
+    AppLocalStorage.init();
   }
 }
