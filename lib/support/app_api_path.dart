e67858e2c85741443d04c309e60c242d45febf34
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-05-26 21:16:48 +0700

    add app api path

---
+++ lib/support/app_api_path.dart
@@ -0,0 +1,5 @@
+class AppApiPath {
+  static const String signInAccount = "/account/signin";
+  static const String signUpAccount = "/account/signup";
+  static const String getUserData = "/account/userdata/";
+}
