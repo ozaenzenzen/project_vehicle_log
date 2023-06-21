0d7aada51b4e4e66cf4bfece0c53a50c7eb636ce
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-21 22:34:13 +0700

    update signin page: implement signin bloc

--- lib/presentation/signin_page.dart
+++ lib/presentation/signin_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:project_vehicle_log/data/model/account/signin_request_models.dart';
+import 'package:project_vehicle_log/presentation/bloc/account_bloc/signin_bloc/signin_bloc.dart';
 import 'package:project_vehicle_log/presentation/home_screen/home_page.dart';
 import 'package:project_vehicle_log/presentation/main_page.dart';
 import 'package:project_vehicle_log/presentation/signup_page.dart';
 import 'package:project_vehicle_log/presentation/widget/app_mainbutton_widget.dart';
 import 'package:project_vehicle_log/presentation/widget/app_textfield_widget.dart';
+import 'package:project_vehicle_log/support/app_color.dart';
+import 'package:project_vehicle_log/support/app_dialog_action.dart';
 import 'package:project_vehicle_log/support/app_info.dart';
 import 'package:project_vehicle_log/support/app_theme.dart';
 
@@ -17,6 +22,8 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailTextFieldController = TextEditingController(text: "");
+  TextEditingController passwordTextFieldController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,36 +55,76 @@ class _SignInPageState extends State<SignInPage> {
               ),
             ),
             SizedBox(height: 20.h),
-            const AppTextFieldWidget(
+            AppTextFieldWidget(
               textFieldTitle: "Email",
               textFieldHintText: "journalist@email.com",
+              controller: emailTextFieldController,
             ),
             SizedBox(height: 10.h),
-            const AppTextFieldWidget(
+            AppTextFieldWidget(
               textFieldTitle: "Passoword",
               textFieldHintText: "*****",
+              controller: passwordTextFieldController,
             ),
             SizedBox(height: 20.h),
-            AppMainButtonWidget(
-              onPressed: () {
-                Get.offAll(
-                  () => const MainPage(),
-                );
+            BlocConsumer<SigninBloc, SigninState>(
+              listener: (context, state) {
+                if (state is SigninFailed) {
+                  AppDialogAction.showPopup(
+                    content: Text(state.errorMessage),
+                    context: context,
+                  );
+                } else if (state is SigninSuccess) {
+                  Get.offAll(
+                    () => const MainPage(),
+                  );
+                }
               },
-              text: "Masuk",
-            ),
-            SizedBox(height: 20.h),
-            Text("Belum Ada Akun?"),
-            SizedBox(height: 20.h),
-            AppMainButtonWidget(
-              onPressed: () {
-                Get.to(
-                  () => const SignUpPage(),
-                );
+              builder: (context, state) {
+                if (state is SigninLoading) {
+                  return SizedBox(
+                    height: 40.h,
+                    width: 40.h,
+                    child: const CircularProgressIndicator(
+                      color: AppColor.primary,
+                    ),
+                  );
+                } else {
+                  return Column(
+                    children: [
+                      AppMainButtonWidget(
+                        onPressed: () {
+                          context.read<SigninBloc>().add(
+                                SigninAction(
+                                  signInRequestModel: SignInRequestModel(
+                                    email: emailTextFieldController.text,
+                                    password: passwordTextFieldController.text,
+                                  ),
+                                ),
+                              );
+                          // Get.offAll(
+                          //   () => const MainPage(),
+                          // );
+                        },
+                        text: "Masuk",
+                      ),
+                      SizedBox(height: 20.h),
+                      Text("Belum Ada Akun?"),
+                      SizedBox(height: 20.h),
+                      AppMainButtonWidget(
+                        onPressed: () {
+                          Get.to(
+                            () => const SignUpPage(),
+                          );
+                        },
+                        text: "Daftar",
+                      ),
+                      SizedBox(height: 20.h),
+                    ],
+                  );
+                }
               },
-              text: "Daftar",
             ),
-            SizedBox(height: 20.h),
             const Spacer(),
             FutureBuilder(
                 future: AppInfo.showAppVersion(),
