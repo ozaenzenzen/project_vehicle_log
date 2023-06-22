20feed8bf5159537f1b9d319a58fe64815479809
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-22 13:27:05 +0700

    update init config: add get storage init

--- lib/init_config.dart
+++ lib/init_config.dart
@@ -1,3 +1,4 @@
+import 'package:get_storage/get_storage.dart';
 import 'package:project_vehicle_log/support/app_connectivity_service.dart';
 import 'package:project_vehicle_log/support/app_info.dart';
 import 'package:project_vehicle_log/support/app_local_storage.dart';
@@ -5,6 +6,7 @@ import 'package:project_vehicle_log/support/app_local_storage.dart';
 class AppInitConfig {
   static Future<void> init() async {
     // AppTheme.appThemeInit();
+    await GetStorage.init();
     AppInfo.appInfoInit();
     AppConnectivityService.init();
     AppLocalStorage.init();
