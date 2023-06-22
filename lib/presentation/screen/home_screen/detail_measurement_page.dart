c1968a5e215be14f98eb00a99b396f32c5ec9e9e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-22 16:05:29 +0700

    update detail measurement page: update data belom selesai

--- lib/presentation/screen/home_screen/detail_measurement_page.dart
+++ lib/presentation/screen/home_screen/detail_measurement_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:project_vehicle_log/data/model/vehicle/get_all_vehicle_data_response_model.dart';
 import 'package:project_vehicle_log/data/vehicle_data_model.dart';
 import 'package:project_vehicle_log/support/app_color.dart';
 import 'package:project_vehicle_log/support/app_theme.dart';
 
 class DetailMeasurementPage extends StatefulWidget {
   // final String title;
-  final VehicleDataModel data;
+  // final VehicleDataModel data;
+  // final DatumVehicle data;
+  final dynamic data;
   final int index;
 
   const DetailMeasurementPage({
@@ -27,7 +30,8 @@ class _DetailMeasurementPageState extends State<DetailMeasurementPage> {
       appBar: AppBar(
         title: Text(
           // "Detail Measurement: ${widget.title}",
-          "${widget.data.vehicleName}: ${widget.data.listControl![widget.index]}",
+          // "${widget.data.vehicleName}: ${widget.data.listControl![widget.index]}",
+           "${widget.data.vehicleName}: ${widget.data.vehicleMeasurementLogModels[widget.index]}",
           textAlign: TextAlign.left,
           style: AppTheme.theme.textTheme.headline3?.copyWith(
             color: AppColor.white,
