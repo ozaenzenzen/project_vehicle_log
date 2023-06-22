648bd8efdf2bb7f451891ede80411300a6c0e652
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-22 15:24:49 +0700

    update notif repository: add func

---
+++ lib/data/repository/notification_repository.dart
@@ -0,0 +1,16 @@
+import 'package:project_vehicle_log/data/model/notification/get_notification_response_model.dart';
+import 'package:project_vehicle_log/env.dart';
+import 'package:project_vehicle_log/support/app_api_path.dart';
+import 'package:project_vehicle_log/support/app_api_service.dart';
+
+class AppNotificationRepository {
+  Future<GetNotificationResponseModel> getNotification(String userId) async {
+    final response = await AppApiService(
+      EnvironmentConfig.baseUrl(),
+    ).call(
+      AppApiPath.getNotification + userId,
+      method: MethodRequest.get,
+    );
+    return GetNotificationResponseModel.fromJson(response.data);
+  }
+}
