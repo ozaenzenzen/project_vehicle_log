91e7bffa977a9700c97d237510716d04b64c8e9f
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-23 01:03:52 +0700

    update create vehicle bloc

--- lib/presentation/bloc/vehicle_bloc/create_vehicle_bloc/create_vehicle_event.dart
+++ lib/presentation/bloc/vehicle_bloc/create_vehicle_bloc/create_vehicle_event.dart
@@ -2,3 +2,11 @@ part of 'create_vehicle_bloc.dart';
 
 @immutable
 abstract class CreateVehicleEvent {}
+
+class CreateVehicleAction extends CreateVehicleEvent {
+  final CreateVehicleRequestModel createVehicleRequestModel;
+
+  CreateVehicleAction({
+    required this.createVehicleRequestModel,
+  });
+}
--- lib/presentation/bloc/vehicle_bloc/create_vehicle_bloc/create_vehicle_state.dart
+++ lib/presentation/bloc/vehicle_bloc/create_vehicle_bloc/create_vehicle_state.dart
@@ -4,3 +4,21 @@ part of 'create_vehicle_bloc.dart';
 abstract class CreateVehicleState {}
 
 class CreateVehicleInitial extends CreateVehicleState {}
+
+class CreateVehicleSuccess extends CreateVehicleState {
+  final CreateVehicleResponseModel createVehicleResponseModel;
+
+  CreateVehicleSuccess({
+    required this.createVehicleResponseModel,
+  });
+}
+
+class CreateVehicleFailed extends CreateVehicleState {
+  final String errorMessage;
+
+  CreateVehicleFailed({
+    required this.errorMessage,
+  });
+}
+
+class CreateVehicleLoading extends CreateVehicleState {}
