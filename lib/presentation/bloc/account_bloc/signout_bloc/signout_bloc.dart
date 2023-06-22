10918b59a36cd6f37585344967d20ea77ea82f18
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-22 13:43:30 +0700

    update signout bloc: add signout action

--- lib/presentation/bloc/account_bloc/signout_bloc/signout_bloc.dart
+++ lib/presentation/bloc/account_bloc/signout_bloc/signout_bloc.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:project_vehicle_log/data/local_repository.dart';
 
 part 'signout_event.dart';
 part 'signout_state.dart';
@@ -7,7 +10,24 @@ part 'signout_state.dart';
 class SignoutBloc extends Bloc<SignoutEvent, SignoutState> {
   SignoutBloc() : super(SignoutInitial()) {
     on<SignoutEvent>((event, emit) {
-      // TODO: implement event handler
+      if (event is SignoutAction) {
+        _signOutAction();
+      }
     });
+  }
+  Future<void> _signOutAction() async {
+    emit(SignoutLoading());
+    await Future.delayed(const Duration(milliseconds: 1000));
+    try {
+      await LocalRepository.removeLocalAccountData();
+      await LocalRepository.signOutSaved();
+      emit(SignoutSuccess());
+    } catch (errorMessage) {
+      emit(
+        SignoutFailed(
+          errorMessage: errorMessage.toString(),
+        ),
+      );
+    }
   }
 }
