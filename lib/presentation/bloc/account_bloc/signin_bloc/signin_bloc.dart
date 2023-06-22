a8d2a859ea6ddd369b649e75e4e828bfbca365b8
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-22 11:49:45 +0700

    update signin bloc: add save local data

--- lib/presentation/bloc/account_bloc/signin_bloc/signin_bloc.dart
+++ lib/presentation/bloc/account_bloc/signin_bloc/signin_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:project_vehicle_log/data/local_repository.dart';
 import 'package:project_vehicle_log/data/model/account/signin_request_models.dart';
 import 'package:project_vehicle_log/data/model/account/signin_response_models.dart';
+import 'package:project_vehicle_log/data/model/account_user_data_model.dart';
 import 'package:project_vehicle_log/data/repository/account_repository.dart';
 
 part 'signin_event.dart';
@@ -23,6 +25,16 @@ class SigninBloc extends Bloc<SigninEvent, SigninState> {
         event.signInRequestModel,
       );
       if (signInResponseModel.status == 200) {
+        AccountDataUserModel data = AccountDataUserModel(
+          userId: signInResponseModel.userdata?.id,
+          name: signInResponseModel.userdata?.name,
+          email: signInResponseModel.userdata?.email,
+          phone: signInResponseModel.userdata?.phone,
+          link: signInResponseModel.userdata?.link,
+          typeuser: signInResponseModel.userdata?.typeuser,
+        );
+        await LocalRepository.saveLocalAccountData(data: data);
+        await LocalRepository.signInSaved();
         emit(
           SigninSuccess(
             userdata: signInResponseModel.userdata!,
