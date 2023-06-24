d4211760fcf6aeb162400970e2c13ff1a4e2ee91
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-24 20:44:10 +0700

    add vehicle local data model

---
+++ lib/data/model/local/vehicle_local_data_model.dart
@@ -0,0 +1,103 @@
+class VehicleLocalDataModel {
+  int? id;
+  int? userId;
+  String? vehicleName;
+  String? vehicleImage;
+  String? year;
+  String? engineCapacity;
+  String? tankCapacity;
+  String? color;
+  String? machineNumber;
+  String? chassisNumber;
+  List<VehicleMeasurementLogModel>? vehicleMeasurementLogModels;
+
+  VehicleLocalDataModel({
+    this.id,
+    this.userId,
+    this.vehicleName,
+    this.vehicleImage,
+    this.year,
+    this.engineCapacity,
+    this.tankCapacity,
+    this.color,
+    this.machineNumber,
+    this.chassisNumber,
+    this.vehicleMeasurementLogModels,
+  });
+
+  factory VehicleLocalDataModel.fromJson(Map<String, dynamic> json) => VehicleLocalDataModel(
+        id: json["id"],
+        userId: json["user_id"],
+        vehicleName: json["vehicle_name"],
+        vehicleImage: json["vehicle_image"],
+        year: json["year"],
+        engineCapacity: json["engine_capacity"],
+        tankCapacity: json["tank_capacity"],
+        color: json["color"],
+        machineNumber: json["machine_number"],
+        chassisNumber: json["chassis_number"],
+        vehicleMeasurementLogModels: List<VehicleMeasurementLogModel>.from(json["vehicle_measurement_log_models"].map((x) => VehicleMeasurementLogModel.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "user_id": userId,
+        "vehicle_name": vehicleName,
+        "vehicle_image": vehicleImage,
+        "year": year,
+        "engine_capacity": engineCapacity,
+        "tank_capacity": tankCapacity,
+        "color": color,
+        "machine_number": machineNumber,
+        "chassis_number": chassisNumber,
+        "vehicle_measurement_log_models": List<dynamic>.from(vehicleMeasurementLogModels!.map((x) => x.toJson())),
+      };
+}
+
+class VehicleMeasurementLogModel {
+  int? id;
+  int? userId;
+  int? vehicleId;
+  String? measurementTitle;
+  String? currentOdo;
+  String? estimateOdoChanging;
+  String? amountExpenses;
+  String? checkpointDate;
+  String? notes;
+
+  VehicleMeasurementLogModel({
+    this.id,
+    this.userId,
+    this.vehicleId,
+    this.measurementTitle,
+    this.currentOdo,
+    this.estimateOdoChanging,
+    this.amountExpenses,
+    this.checkpointDate,
+    this.notes,
+  });
+
+  factory VehicleMeasurementLogModel.fromJson(Map<String, dynamic> json) => VehicleMeasurementLogModel(
+        id: json["id"],
+        userId: json["user_id"],
+        vehicleId: json["vehicle_id"],
+        measurementTitle: json["measurement_title"],
+        currentOdo: json["current_odo"],
+        estimateOdoChanging: json["estimate_odo_changing"],
+        amountExpenses: json["amount_expenses"],
+        checkpointDate: json["checkpoint_date"],
+        notes: json["notes"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "user_id": userId,
+        "vehicle_id": vehicleId,
+        "measurement_title": measurementTitle,
+        "current_odo": currentOdo,
+        "estimate_odo_changing": estimateOdoChanging,
+        "amount_expenses": amountExpenses,
+        "checkpoint_date": checkpointDate,
+        "notes": notes,
+      };
+}
