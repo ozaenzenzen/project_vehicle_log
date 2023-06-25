a705a2a26996c6f579359e2efe470d2321c9de0c
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-25 16:06:52 +0700

    update stats page: implement get all vehicle bloc + categorized

--- lib/presentation/screen/stats_screen/stats_page.dart
+++ lib/presentation/screen/stats_screen/stats_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:project_vehicle_log/data/dummy_data.dart';
+import 'package:project_vehicle_log/data/local_repository/vehicle_local_repository.dart';
+import 'package:project_vehicle_log/data/model/remote/vehicle/get_all_vehicle_data_response_model.dart';
 import 'package:project_vehicle_log/data/vehicle_data_model.dart';
+import 'package:project_vehicle_log/presentation/bloc/vehicle_bloc/get_all_vehicle_bloc/get_all_vehicle_bloc.dart';
 import 'package:project_vehicle_log/presentation/screen/home_screen/detail_measurement_page.dart';
 import 'package:project_vehicle_log/presentation/widget/app_container_box_widget.dart';
+import 'package:project_vehicle_log/presentation/widget/app_loading_indicator.dart';
 import 'package:project_vehicle_log/support/app_color.dart';
 import 'package:project_vehicle_log/support/app_theme.dart';
 
@@ -16,11 +21,37 @@ class StatsPage extends StatefulWidget {
 }
 
 class _StatsPageState extends State<StatsPage> {
-  String? dropDownValue = DummyData.dummyData.first.vehicleName;
+  // String? dropDownValue = DummyData.dummyData.first.vehicleName;
   // VehicleDataModel dropDownValue = DummyData.dummyData.first;
-  int indexChosen = 0;
+  List<DatumVehicle>? dataStats;
+  String? dropDownValue = "";
+  // int indexChosen = 0;
 
   FocusNode dropDownFocusNode = FocusNode();
+
+  late GetAllVehicleBloc getAllVehicleBloc;
+
+  @override
+  void initState() {
+    getAllVehicleBloc = BlocProvider.of(context)
+      ..add(
+        GetAllVehicleDataFromLocalAction(
+          vehicleLocalRepository: VehicleLocalRepository(),
+        ),
+      );
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    getAllVehicleBloc = BlocProvider.of(context)
+      ..add(
+        GetAllVehicleDataFromLocalAction(
+          vehicleLocalRepository: VehicleLocalRepository(),
+        ),
+      );
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,97 +83,178 @@ class _StatsPageState extends State<StatsPage> {
                   fontWeight: FontWeight.w500,
                 ),
               ),
-              SizedBox(height: 20.h),
-              DropdownButtonFormField<String>(
-                focusNode: dropDownFocusNode,
-                items: DummyData.dummyData.map((VehicleDataModel e) {
-                  return DropdownMenuItem(
-                    value: e.vehicleName,
-                    child: Text(
-                      "${e.vehicleName}",
-                      style: AppTheme.theme.textTheme.headlineSmall?.copyWith(
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    dropDownValue = value!;
-                  });
-                  dropDownFocusNode.unfocus();
+              BlocListener<GetAllVehicleBloc, GetAllVehicleState>(
+                bloc: getAllVehicleBloc,
+                listener: (context, state) {
+                  // debugPrint("listen something $state");
+                  if (state is GetAllVehicleSuccess) {
+                    dropDownValue = state.getAllVehicleDataResponseModel.data!.first.vehicleName;
+                  }
                 },
-                value: dropDownValue,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-              ),
-              ListView.separated(
-                padding: EdgeInsets.symmetric(
-                  vertical: 20.h,
-                ),
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: DummyData
-                    .dummyData[DummyData.dummyData.indexWhere((element) {
-                  return element.vehicleName == dropDownValue;
-                })]
-                    .listControl!
-                    .length,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      Get.to(
-                        () => DetailMeasurementPage(
-                          data: DummyData.dummyData[DummyData.dummyData.indexWhere((element) {
-                  return element.vehicleName == dropDownValue;
-                })],
-                          index: index,
-                        ),
+                child: BlocBuilder<GetAllVehicleBloc, GetAllVehicleState>(
+                  builder: (context, state) {
+                    if (state is GetAllVehicleLoading) {
+                      return const Center(
+                        child: AppLoadingIndicator(),
                       );
-                    },
-                    child: AppContainerBoxWidget(
-                      height: 200.h,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.water_drop,
-                              size: 90.h,
-                              color: AppColor.primary,
-                            ),
-                            SizedBox(height: 10.h),
-                            Text(
-                              // "Oil",
-                              DummyData
-                                  .dummyData[DummyData.dummyData.indexWhere((element) {
-                                return element.vehicleName == dropDownValue;
-                              })]
-                                  .listControl![index],
-                              style: AppTheme.theme.textTheme.headlineSmall?.copyWith(
-                                fontWeight: FontWeight.w600,
+                    } else if (state is GetAllVehicleFailed) {
+                      return Text(state.errorMessage);
+                    } else if (state is GetAllVehicleSuccess) {
+                      // dataStats = state.getAllVehicleDataResponseModel.data;
+                      // dropDownValue = dataStats!.first.vehicleName;
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 20.h),
+                          DropdownButtonFormField<String>(
+                            focusNode: dropDownFocusNode,
+                            // items: DummyData.dummyData.map((VehicleDataModel e) {
+                            //   return DropdownMenuItem(
+                            //     value: e.vehicleName,
+                            //     child: Text(
+                            //       "${e.vehicleName}",
+                            //       style: AppTheme.theme.textTheme.headlineSmall?.copyWith(
+                            //         fontWeight: FontWeight.w500,
+                            //       ),
+                            //     ),
+                            //   );
+                            // }).toList(),
+                            items: state.getAllVehicleDataResponseModel.data?.map((DatumVehicle e) {
+                              return DropdownMenuItem(
+                                value: e.vehicleName,
+                                child: Text(
+                                  e.vehicleName,
+                                  style: AppTheme.theme.textTheme.headlineSmall?.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              debugPrint("on changed $value");
+                              setState(() {
+                                dropDownValue = value!;
+                              });
+                              dropDownFocusNode.unfocus();
+                            },
+                            // value: dropDownValue,
+                            value: state.getAllVehicleDataResponseModel.data!.first.vehicleName,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
                               ),
                             ),
-                          ],
-                        ),
-                        // child: Text(
-                        //   DummyData.dummyData[0].listControl![index],
-                        //   style: AppTheme.theme.textTheme.headline5?.copyWith(
-                        //     // color: AppColor.text_4,
-                        //     color: Colors.black38,
-                        //     fontWeight: FontWeight.w500,
-                        //   ),
-                        // ),
-                      ),
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(height: 20.h);
-                },
+                          ),
+                          ListView.separated(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 20.h,
+                            ),
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            // itemCount: DummyData
+                            //     .dummyData[DummyData.dummyData.indexWhere((element) {
+                            //   return element.vehicleName == dropDownValue;
+                            // })]
+                            //     .listControl!
+                            //     .length,
+                            itemCount: state
+                                .getAllVehicleDataResponseModel
+                                .data![(state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                          return element.vehicleName == dropDownValue;
+                                        }) <
+                                        0)
+                                    ? 0
+                                    : state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                        return element.vehicleName == dropDownValue;
+                                      })]
+                                .categorizedData!
+                                .length,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  // Get.to(
+                                  //   () => DetailMeasurementPage(
+                                  //     data: DummyData.dummyData[DummyData.dummyData.indexWhere((element) {
+                                  //       return element.vehicleName == dropDownValue;
+                                  //     })],
+                                  //     index: index,
+                                  //   ),
+                                  // );
+                                  Get.to(
+                                    () => DetailMeasurementPage(
+                                      // data: state.getAllVehicleDataResponseModel.data![index],
+                                      data: state.getAllVehicleDataResponseModel.data![(state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                                return element.vehicleName == dropDownValue;
+                                              }) <
+                                              0)
+                                          ? 0
+                                          : state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                              return element.vehicleName == dropDownValue;
+                                            })],
+                                      index: index,
+                                    ),
+                                  );
+                                },
+                                child: AppContainerBoxWidget(
+                                  height: 200.h,
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.water_drop,
+                                          size: 90.h,
+                                          color: AppColor.primary,
+                                        ),
+                                        SizedBox(height: 10.h),
+                                        Text(
+                                          // "Oil",
+                                          // DummyData
+                                          //     .dummyData[DummyData.dummyData.indexWhere((element) {
+                                          //   return element.vehicleName == dropDownValue;
+                                          // })]
+                                          //     .listControl![index],
+                                          state
+                                              .getAllVehicleDataResponseModel
+                                              .data![(state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                                        return element.vehicleName == dropDownValue;
+                                                      }) <
+                                                      0)
+                                                  ? 0
+                                                  : state.getAllVehicleDataResponseModel.data!.indexWhere((element) {
+                                                      return element.vehicleName == dropDownValue;
+                                                    })]
+                                              .categorizedData![index]
+                                              .measurementTitle!,
+                                          style: AppTheme.theme.textTheme.headlineSmall?.copyWith(
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    // child: Text(
+                                    //   DummyData.dummyData[0].listControl![index],
+                                    //   style: AppTheme.theme.textTheme.headline5?.copyWith(
+                                    //     // color: AppColor.text_4,
+                                    //     color: Colors.black38,
+                                    //     fontWeight: FontWeight.w500,
+                                    //   ),
+                                    // ),
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) {
+                              return SizedBox(height: 20.h);
+                            },
+                          ),
+                        ],
+                      );
+                    } else {
+                      return Text("data is null");
+                    }
+                  },
+                ),
               ),
             ],
           ),
