36109337991204564ae597d775fb2a9259c19db1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-25 16:05:43 +0700

    fix detail measurement page

--- lib/presentation/screen/home_screen/detail_measurement_page.dart
+++ lib/presentation/screen/home_screen/detail_measurement_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:project_vehicle_log/data/model/remote/vehicle/get_all_vehicle_data_response_model.dart';
 import 'package:project_vehicle_log/support/app_color.dart';
 import 'package:project_vehicle_log/support/app_theme.dart';
 
 class DetailMeasurementPage extends StatefulWidget {
   // final String title;
   // final VehicleDataModel data;
-  // final DatumVehicle data;
-  final dynamic data;
+  final DatumVehicle data;
+  // final dynamic data;
+  // final CategorizedVehicleLogData data;
   final int index;
 
   const DetailMeasurementPage({
@@ -29,7 +31,7 @@ class _DetailMeasurementPageState extends State<DetailMeasurementPage> {
         title: Text(
           // "Detail Measurement: ${widget.title}",
           // "${widget.data.vehicleName}: ${widget.data.listControl![widget.index]}",
-           "${widget.data.vehicleName}: ${widget.data.vehicleMeasurementLogModels[widget.index]}",
+           "${widget.data.vehicleName}: ${widget.data.categorizedData![widget.index].measurementTitle}",
           textAlign: TextAlign.left,
           style: AppTheme.theme.textTheme.headline3?.copyWith(
             color: AppColor.white,
