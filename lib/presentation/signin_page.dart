b5f4a663d2c07b63fb64fd7366ad0c9bf022404c
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-26 19:44:42 +0700

    update signin page: improve app version view

--- lib/presentation/signin_page.dart
+++ lib/presentation/signin_page.dart
@@ -36,139 +36,150 @@ class _SignInPageState extends State<SignInPage> {
         // appBar: AppBar(),
         body: Container(
           padding: EdgeInsets.all(16.h),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Stack(
             children: [
-              const Spacer(),
-              // SizedBox(height: 100.h),
-              // Align(
-              //   alignment: Alignment.center,
-              //   child: Text(
-              //     "Masuk",
-              //     style: AppTheme.theme.textTheme.headline2?.copyWith(
-              //       fontWeight: FontWeight.w600,
-              //     ),
-              //   ),
-              // ),
-              // SizedBox(height: 100.h),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Masuk",
-                  style: AppTheme.theme.textTheme.headline2?.copyWith(
-                    fontWeight: FontWeight.w600,
-                  ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FutureBuilder(
+                      future: AppInfo.showAppVersion(),
+                      builder: (context, snapshot) {
+                        return Text(
+                          // "Vehicle Log Apps Version 1.0.0+1",
+                          // "Vehicle Log Apps Version ${AppInfo.appVersion}",
+                          "Vehicle Log Apps Version ${snapshot.data}",
+                          style: AppTheme.theme.textTheme.caption?.copyWith(
+                            fontSize: 10.sp,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        );
+                      }),
                 ),
               ),
-              SizedBox(height: 20.h),
-              AppTextFieldWidget(
-                textFieldTitle: "Email",
-                textFieldHintText: "journalist@email.com",
-                controller: emailTextFieldController,
-              ),
-              SizedBox(height: 10.h),
-              AppTextFieldWidget(
-                textFieldTitle: "Password",
-                textFieldHintText: "*****",
-                controller: passwordTextFieldController,
-                obscureText: true,
-              ),
-              SizedBox(height: 20.h),
-              BlocConsumer<SigninBloc, SigninState>(
-                listener: (context, state) {
-                  if (state is SigninFailed) {
-                    AppDialogAction.showPopup(
-                      content: Column(
-                        children: [
-                          Icon(
-                            Icons.close,
-                            color: AppColor.error,
-                            size: 80.h,
-                          ),
-                          SizedBox(height: 20.h),
-                          Text(
-                            "Error",
-                            style: AppTheme.theme.textTheme.headline4?.copyWith(
-                              // color: AppColor.text_4,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(height: 10.h),
-                          Text(
-                            state.errorMessage,
-                            style: AppTheme.theme.textTheme.headline6?.copyWith(
-                              // color: AppColor.text_4,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  // SizedBox(height: 100.h),
+                  // Align(
+                  //   alignment: Alignment.center,
+                  //   child: Text(
+                  //     "Masuk",
+                  //     style: AppTheme.theme.textTheme.headline2?.copyWith(
+                  //       fontWeight: FontWeight.w600,
+                  //     ),
+                  //   ),
+                  // ),
+                  // SizedBox(height: 100.h),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Masuk",
+                      style: AppTheme.theme.textTheme.headline2?.copyWith(
+                        fontWeight: FontWeight.w600,
                       ),
-                      context: context,
-                    );
-                  } else if (state is SigninSuccess) {
-                    Get.offAll(
-                      () => const MainPage(),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is SigninLoading) {
-                    return const AppLoadingIndicator();
-                  } else {
-                    return Column(
-                      children: [
-                        AppMainButtonWidget(
-                          onPressed: () {
-                            context.read<SigninBloc>().add(
-                                  SigninAction(
-                                    signInRequestModel: SignInRequestModel(
-                                      email: emailTextFieldController.text,
-                                      password: passwordTextFieldController.text,
-                                    ),
-                                    appVehicleReposistory: AppVehicleReposistory(),
-                                    vehicleLocalRepository: VehicleLocalRepository(),
-                                  ),
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+                  AppTextFieldWidget(
+                    textFieldTitle: "Email",
+                    textFieldHintText: "journalist@email.com",
+                    controller: emailTextFieldController,
+                  ),
+                  SizedBox(height: 10.h),
+                  AppTextFieldWidget(
+                    textFieldTitle: "Password",
+                    textFieldHintText: "*****",
+                    controller: passwordTextFieldController,
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 20.h),
+                  BlocConsumer<SigninBloc, SigninState>(
+                    listener: (context, state) {
+                      if (state is SigninFailed) {
+                        AppDialogAction.showPopup(
+                          content: Column(
+                            children: [
+                              Icon(
+                                Icons.close,
+                                color: AppColor.error,
+                                size: 80.h,
+                              ),
+                              SizedBox(height: 20.h),
+                              Text(
+                                "Error",
+                                style: AppTheme.theme.textTheme.headline4?.copyWith(
+                                  // color: AppColor.text_4,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(height: 10.h),
+                              Text(
+                                state.errorMessage,
+                                style: AppTheme.theme.textTheme.headline6?.copyWith(
+                                  // color: AppColor.text_4,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                          context: context,
+                        );
+                      } else if (state is SigninSuccess) {
+                        Get.offAll(
+                          () => const MainPage(),
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      if (state is SigninLoading) {
+                        return const AppLoadingIndicator();
+                      } else {
+                        return Column(
+                          children: [
+                            AppMainButtonWidget(
+                              onPressed: () {
+                                context.read<SigninBloc>().add(
+                                      SigninAction(
+                                        signInRequestModel: SignInRequestModel(
+                                          email: emailTextFieldController.text,
+                                          password: passwordTextFieldController.text,
+                                        ),
+                                        appVehicleReposistory: AppVehicleReposistory(),
+                                        vehicleLocalRepository: VehicleLocalRepository(),
+                                      ),
+                                    );
+                                // Get.offAll(
+                                //   () => const MainPage(),
+                                // );
+                              },
+                              text: "Masuk",
+                            ),
+                            SizedBox(height: 20.h),
+                            Text("Belum Ada Akun?"),
+                            SizedBox(height: 20.h),
+                            AppMainButtonWidget(
+                              onPressed: () {
+                                Get.to(
+                                  () => const SignUpPage(),
                                 );
-                            // Get.offAll(
-                            //   () => const MainPage(),
-                            // );
-                          },
-                          text: "Masuk",
-                        ),
-                        SizedBox(height: 20.h),
-                        Text("Belum Ada Akun?"),
-                        SizedBox(height: 20.h),
-                        AppMainButtonWidget(
-                          onPressed: () {
-                            Get.to(
-                              () => const SignUpPage(),
-                            );
-                          },
-                          text: "Daftar",
-                        ),
-                        SizedBox(height: 20.h),
-                      ],
-                    );
-                  }
-                },
+                              },
+                              text: "Daftar",
+                            ),
+                            SizedBox(height: 20.h),
+                          ],
+                        );
+                      }
+                    },
+                  ),
+                  const Spacer(),
+                ],
               ),
-              const Spacer(),
-              FutureBuilder(
-                  future: AppInfo.showAppVersion(),
-                  builder: (context, snapshot) {
-                    return Text(
-                      // "Vehicle Log Apps Version 1.0.0+1",
-                      // "Vehicle Log Apps Version ${AppInfo.appVersion}",
-                      "Vehicle Log Apps Version ${snapshot.data}",
-                      style: AppTheme.theme.textTheme.caption?.copyWith(
-                        fontSize: 10.sp,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    );
-                  }),
             ],
           ),
         ),
