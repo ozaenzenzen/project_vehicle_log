68e983e8c67dc1421e466ae343b12166a6755922
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-06-29 21:13:52 +0700

    update create log vehicle bloc

--- lib/presentation/bloc/vehicle_bloc/create_log_vehicle_bloc/create_log_vehicle_event.dart
+++ lib/presentation/bloc/vehicle_bloc/create_log_vehicle_bloc/create_log_vehicle_event.dart
@@ -2,3 +2,11 @@ part of 'create_log_vehicle_bloc.dart';
 
 @immutable
 abstract class CreateLogVehicleEvent {}
+
+class CreateLogVehicleAction extends CreateLogVehicleEvent {
+  final CreateLogVehicleRequestModel createLogVehicleRequestModel;
+
+  CreateLogVehicleAction({
+    required this.createLogVehicleRequestModel,
+  });
+}
--- lib/presentation/bloc/vehicle_bloc/create_log_vehicle_bloc/create_log_vehicle_state.dart
+++ lib/presentation/bloc/vehicle_bloc/create_log_vehicle_bloc/create_log_vehicle_state.dart
@@ -4,3 +4,21 @@ part of 'create_log_vehicle_bloc.dart';
 abstract class CreateLogVehicleState {}
 
 class CreateLogVehicleInitial extends CreateLogVehicleState {}
+
+class CreateLogVehicleLoading extends CreateLogVehicleState {}
+
+class CreateLogVehicleSuccess extends CreateLogVehicleState {
+  final CreateLogVehicleResponseModel createLogVehicleResponseModel;
+
+  CreateLogVehicleSuccess({
+    required this.createLogVehicleResponseModel,
+  });
+}
+
+class CreateLogVehicleFailed extends CreateLogVehicleState {
+  final String errorMessage;
+
+  CreateLogVehicleFailed({
+    required this.errorMessage,
+  });
+}
