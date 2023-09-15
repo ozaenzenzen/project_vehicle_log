11df77e21f33c93d560a7314b87ce365edaf0825
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-09-15 22:33:06 +0700

    update signin page ui

--- lib/presentation/signin_page.dart
+++ lib/presentation/signin_page.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:project_vehicle_log/data/local_repository/vehicle_local_repository.dart';
 import 'package:project_vehicle_log/data/model/remote/account/signin_request_models.dart';
 import 'package:project_vehicle_log/data/repository/vehicle_repository.dart';
 import 'package:project_vehicle_log/presentation/bloc/account_bloc/signin_bloc/signin_bloc.dart';
 import 'package:project_vehicle_log/presentation/main_page.dart';
+import 'package:project_vehicle_log/presentation/screen/forgot_password_screen/forgot_password_screen.dart';
 import 'package:project_vehicle_log/presentation/signup_page.dart';
 import 'package:project_vehicle_log/presentation/widget/app_loading_indicator.dart';
 import 'package:project_vehicle_log/presentation/widget/app_mainbutton_widget.dart';
@@ -26,6 +28,9 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   TextEditingController emailTextFieldController = TextEditingController(text: "");
   TextEditingController passwordTextFieldController = TextEditingController(text: "");
+
+  bool keepLogin = false;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -96,38 +101,64 @@ class _SignInPageState extends State<SignInPage> {
                     controller: passwordTextFieldController,
                     obscureText: true,
                   ),
+                  SizedBox(height: 10.h),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            keepLogin = !keepLogin;
+                          });
+                        },
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 24.h,
+                              width: 24.h,
+                              child: Checkbox(
+                                value: keepLogin,
+                                onChanged: (onChanged) {
+                                  setState(() {
+                                    keepLogin = !keepLogin;
+                                  });
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            Text(
+                              "Remember Me",
+                              style: AppTheme.theme.textTheme.headline4?.copyWith(
+                                fontSize: 14.sp,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      InkWell(
+                        onTap: () {
+                          Get.to(() => const ForgotPasswordScreen());
+                        },
+                        child: Text(
+                          "Forgot Password",
+                          style: AppTheme.theme.textTheme.headline4?.copyWith(
+                            fontSize: 14.sp,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                   SizedBox(height: 20.h),
                   BlocConsumer<SigninBloc, SigninState>(
                     listener: (context, state) {
                       if (state is SigninFailed) {
-                        AppDialogAction.showPopup(
-                          content: Column(
-                            children: [
-                              Icon(
-                                Icons.close,
-                                color: AppColor.error,
-                                size: 80.h,
-                              ),
-                              SizedBox(height: 20.h),
-                              Text(
-                                "Error",
-                                style: AppTheme.theme.textTheme.headline4?.copyWith(
-                                  // color: AppColor.text_4,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              SizedBox(height: 10.h),
-                              Text(
-                                state.errorMessage,
-                                style: AppTheme.theme.textTheme.headline6?.copyWith(
-                                  // color: AppColor.text_4,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
+                        AppDialogAction.showFailedPopup(
+                          title: 'Terjadi kesalahan',
+                          description: state.errorMessage,
+                          buttonTitle: 'Kembali',
                           context: context,
                         );
                       } else if (state is SigninSuccess) {
@@ -161,7 +192,14 @@ class _SignInPageState extends State<SignInPage> {
                               text: "Masuk",
                             ),
                             SizedBox(height: 20.h),
-                            Text("Belum Ada Akun?"),
+                            Text(
+                              "Belum Ada Akun?",
+                              style: GoogleFonts.inter(
+                                color: Colors.black,
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
                             SizedBox(height: 20.h),
                             AppMainButtonWidget(
                               onPressed: () {
