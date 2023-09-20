04b374672586f6119c697bce98a3969bc1965744
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-09-21 01:27:07 +0700

    update account local data model

--- lib/data/model/local/account_user_data_model.dart
+++ lib/data/model/local/account_user_data_model.dart
@@ -4,24 +4,27 @@ class AccountDataUserModel {
     this.name,
     this.email,
     this.phone,
-    this.link,
-    this.typeuser,
+    // this.link,
+    // this.typeuser,
+    this.token,
   });
 
   int? userId;
   String? name;
   String? email;
   String? phone;
-  String? link;
-  int? typeuser;
+  // String? link;
+  // int? typeuser;
+  String? token;
 
   factory AccountDataUserModel.fromJson(Map<String, dynamic> json) => AccountDataUserModel(
         userId: json["user_id"],
         name: json["name"],
         email: json["email"],
         phone: json["phone"],
-        link: json["link"],
-        typeuser: json["typeuser"],
+        // link: json["link"],
+        // typeuser: json["typeuser"],
+        token: json["token"],
         // name: json["name"] ?? "",
         // email: json["email"] ?? "",
         // phone: json["phone"] ?? "",
@@ -34,8 +37,9 @@ class AccountDataUserModel {
         "name": name,
         "email": email,
         "phone": phone,
-        "link": link,
-        "typeuser": typeuser,
+        // "link": link,
+        // "typeuser": typeuser,
+        "token": token,
       };
 }
 
