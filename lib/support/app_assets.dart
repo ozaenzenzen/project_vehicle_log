797dc1525c0135d24aec5f5cead2c9bdf8e5d623
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-10-01 00:59:35 +0700

    handle empty state with new asset in detail vehicle

--- lib/support/app_assets.dart
+++ lib/support/app_assets.dart
@@ -3,4 +3,6 @@ class AppAssets {
   static String iconPopupKonfirmasi = "assets/icon_popup_konfirmasi.png";
   static String iconPopupSuccess = "assets/icon_popup_success.png";
   static String iconPopupWarning = "assets/icon_popup_warning.png";
+  static String imgEmptyState = "assets/img_empty_state.png";
+  static String imgEmptyStateBlue = "assets/img_empty_state_blue.png";
 }
